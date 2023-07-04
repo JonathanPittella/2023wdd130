e06b3ec134f75c5ca060f017c68d61d45bc58d29
JonathanPittella <jonathanpittella22@gmail.com>
2023-07-03 22:07:13 -0300

    update

---
+++ mysite/js/myscript1.js
@@ -0,0 +1,26 @@
+// When the user scrolls the page, execute the scrollFunction
+window.onscroll = function() {
+  scrollProgressBar();
+  scrollFunction();
+};
+
+function scrollProgressBar() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+function scrollFunction() {
+  let mybutton = document.getElementById("myBtn");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
